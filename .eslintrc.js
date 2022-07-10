7b4d6aa07e6f1a863e2123137efc84da78d4df5a
Marcin Pawlicki <marcin.pawlicki@netguru.com>
2022-07-10 19:38:06 +0200

    fix: rules, removed garbage

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,20 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react-native'],
+  rules: {
+    semi: 'off',
+    'no-throw-literal': 'error',
+    'react-native/no-unused-styles': 'error',
+    'react-native/split-platform-components': 'error',
+    'react-native/no-inline-styles': 'error',
+    'react-native/no-color-literals': 'error',
+    'react-native/no-single-element-style-arrays': 'error',
+    'no-console': ['warn', {allow: ['warn', 'error']}],
+    'no-shadow': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
